7cb48c183aa452a45ec66e8cbf42a104f2614a34
Shalini Bhaskara <shbhaskara@cs.stonybrook.edu>
2017-09-25 22:26:02 -0400

    minor change

--- include/sys/pci.h
+++ include/sys/pci.h
@@ -5,7 +5,5 @@ uint32_t inl(uint16_t port);
 void outl( uint16_t port, uint32_t val);
 uint16_t pciConfigReadWord (uint8_t bus, uint8_t device, uint8_t func, uint8_t offset);
 int pciCheckForAHCI(uint8_t bus, uint8_t device, uint8_t f);
-//int pciCheckForAHCI(uint8_t bus, uint8_t device);
-// static int check_type(hba_port_t *port);
 void probe_port(hba_mem_t *abar);
 void checkAllBuses();
