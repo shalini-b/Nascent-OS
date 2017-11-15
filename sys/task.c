814524370ba070906ffd12005760f00b0f8a8d2a
Shalini Bhaskara <shbhaskara@cs.stonybrook.edu>
2017-11-15 02:59:40 -0500

    Initial context switching task code

---
+++ sys/task.c
@@ -0,0 +1,47 @@
+#include <sys/task.h>
+#include <sys/virmem.h>
+#include <sys/kprintf.h>
+ 
+static Task *runningTask;
+static Task mainTask;
+static Task otherTask;
+ 
+static void otherMain() {
+    kprintf("Hello multitasking world!");
+    yield();
+}
+ 
+void init() {
+    // Get EFLAGS and CR3
+    __asm__ __volatile__ ("movq %%cr3, %%rax; movq %%rax, %0;":"=m"(mainTask.regs.cr3)::"%rax");
+    __asm__ __volatile__ ("pushfq; movq (%%rsp), %%rax; movq %%rax, %0; popfq;":"=m"(mainTask.regs.eflags)::"%rax");
+ 
+    createTask(&otherTask, otherMain, mainTask.regs.eflags, (uint64_t *)mainTask.regs.cr3);
+    mainTask.next = &otherTask;
+    otherTask.next = &mainTask;
+ 
+    runningTask = &mainTask;
+}
+ 
+void createTask(Task *task, void (*main)(), uint64_t flags, uint64_t *pagedir) {
+    task->regs.rbx = 0;
+    task->regs.rbp = 0;
+    task->regs.rdi= 0;
+    task->regs.rsi = 0;
+    task->regs.rsp = 0;
+    task->regs.r12 = 0;
+    task->regs.r13 = 0;
+    task->regs.r14 = 0;
+    task->regs.r15 = 0;
+    task->regs.eflags = flags;
+    task->regs.rip = (uint64_t) main;
+    task->regs.cr3 = (uint64_t) pagedir;
+    task->regs.rsp = (uint64_t) fetch_free_page() + 0x1000;
+    task->next = 0;
+}
+ 
+void yield() {
+    Task *last = runningTask;
+    runningTask = runningTask->next;
+    switchTask(&last->regs, &runningTask->regs);
+}
